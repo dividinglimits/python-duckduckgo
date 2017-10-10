f7133ca62ab59badab80d9baf7a9d26672c3c45d
Ammon Smith <ammon.i.smith@gmail.com>
2017-10-09 22:42:54 -0700

    Add logging.

--- duckduckgo/query.py
+++ duckduckgo/query.py
@@ -6,6 +6,7 @@
 #
 # See LICENSE for terms of usage, modification and redistribution.
 
+import logging
 import urllib.parse
 from typing import Tuple
 
@@ -17,6 +18,8 @@ from .models import Results
 
 DEFAULT_USER_AGENT = f'python-duckduckgo {__version__}'
 DEFAULT_PRIORITIES = ('answer', 'abstract', 'related.0', 'definition')
+
+logger = logging.getLogger('duckduckgo')
 
 @rate_limited(1)
 async def query(q: str,
@@ -38,6 +41,11 @@ async def query(q: str,
     Any other keyword arguments are passed directly to DuckDuckGo as URL params.
     """
 
+    logger.info(f"Performing DDG query: '{q}'")
+    logger.debug(f"Safesearch: {safesearch}")
+    logger.debug(f"HTML: {html}")
+    logger.debug(f"Meanings: {meanings}")
+
     safesearch = '1' if safesearch else '-1'
     html = '0' if html else '1'
     meanings = '0' if meanings else '1'
@@ -50,6 +58,8 @@ async def query(q: str,
         'd': meanings,
     }
     params.update(kwargs)
+    logger.debug(f"Full parameters: {params}")
+
     encparams = urllib.parse.urlencode(params)
     url = f'http://api.duckduckgo.com/?{encparams}'
 
@@ -57,7 +67,10 @@ async def query(q: str,
         async with cs.get(url, headers={'User-Agent': useragent}) as req:
             response = await req.json(content_type='application/x-javascript')
             if response is None:
+                logger.error("Response is 'None'")
                 raise ValueError("Failed to decode JSON response")
+
+    logger.debug("Response is {response}")
     return Results(response)
 
 async def get_zci(q: str,
@@ -71,6 +84,10 @@ async def get_zci(q: str,
     passed to query. This method will fall back to 'Sorry, no results.'
     if it cannot find anything.'''
 
+    logger.info(f"Performing DDG ZCI: '{q}'")
+    logger.debug(f"Web fallback: {web_fallback}")
+    logger.debug(f"Use URLs: {urls}")
+
     ddg = await query(f'\\{q}', **kwargs)
     response = ''
 
@@ -82,26 +99,33 @@ async def get_zci(q: str,
         result = getattr(ddg, type)
         if index is not None:
             if not hasattr(result, '__getitem__'):
+                logger.error("Result is not indexable!")
                 raise TypeError(f'{type} field is not indexable')
+
             result = result[index] if len(result) > index else None
+
         if not result:
             continue
-
-        if result.text:
+        elif result.text:
+            logger.debug(f"Result has text: {result.text}")
             response = result.text
-        if result.text and hasattr(result, 'url') and urls:
-            if result.url:
+
+            if getattr(result, 'url', None) and urls:
+                logger.debug(f"Result has url: {result.url}")
                 response += f' ({result.url})'
-        if response:
+
             break
 
     # If there still isn't anything, try to get the first web result
+    logger.debug("Trying web fallback...")
     if not response and web_fallback:
         if ddg.redirect.url:
             response = ddg.redirect.url
 
     # Final fallback
+    logger.info("No results!")
     if not response:
         response = 'Sorry, no results.'
 
+    logger.debug(f"Final response: {response!r}")
     return response
