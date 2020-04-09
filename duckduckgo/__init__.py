a4d772d5958a1b1db17642bc8dc0cfc02d197a54
Ignas Maslinskas <ignasmaslinskas@gmail.com>
2020-04-09 20:56:35 +0300

    Add to init

--- duckduckgo/__init__.py
+++ duckduckgo/__init__.py
@@ -7,13 +7,14 @@
 # See LICENSE for terms of usage, modification and redistribution.
 
 from ._version import __version__
-from .query import DuckDuckGoError, query, zci
+from .query import DuckDuckGoError, query, zci, zci_extra
 from .ratelimit import Ratelimit
 
 __all__ = [
     'DuckDuckGoError',
     'query',
     'zci',
+    'zci_extra',
     'Ratelimit',
     '__version__',
 ]
