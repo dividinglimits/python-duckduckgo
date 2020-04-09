92241e4d94ebeb88d827ba5dcbb302411a42467b
Ignas Maslinskas <ignasmaslinskas@gmail.com>
2020-04-09 20:49:17 +0300

    Add method with extra return information

--- duckduckgo/query.py
+++ duckduckgo/query.py
@@ -129,3 +129,67 @@ async def zci(q: str,
 
     logger.debug(f"Final response: {response!r}")
     return response
+
+async def zci_extra(q: str,
+        web_fallback: bool = True,
+        priority: Tuple[str] = DEFAULT_PRIORITIES,
+        urls: bool = True,
+        **kwargs) -> Tuple[str, bool, bool]:
+    '''A helper method to get a single (and hopefully the best) ZCI result.
+    priority=list can be used to set the order in which fields will be checked for answers.
+    Use web_fallback=True to fall back to grabbing the first web result.
+    passed to query. This method will fall back to 'Sorry, no results.'
+    if it cannot find anything. Returns a tuple with [result, result_found, is_fallback] which
+    allows to determine how the result was retrieved.'''
+
+    logger.info(f"Performing DDG ZCI: '{q}'")
+    logger.debug(f"Web fallback: {web_fallback}")
+    logger.debug(f"Use URLs: {urls}")
+
+    ddg = await query(f'\\{q}', **kwargs)
+    response = ''
+    found = True
+    is_fallback = False
+
+    for p in priority:
+        ps = p.split('.')
+        type = ps[0]
+        index = int(ps[1]) if len(ps) > 1 else None
+
+        result = getattr(ddg, type)
+        if index is not None:
+            if not hasattr(result, '__getitem__'):
+                logger.error("Result is not indexable!")
+                raise TypeError(f'{type} field is not indexable')
+
+            result = result[index] if len(result) > index else None
+
+        if not result:
+            continue
+        elif result.text:
+            logger.debug(f"Result has text: {result.text}")
+            response = result.text
+
+            if getattr(result, 'url', None) and urls:
+                logger.debug(f"Result has url: {result.url}")
+                response += f' ({result.url})'
+
+            break
+
+    # If there still isn't anything, try to get the first web result
+    logger.debug("Trying web fallback...")
+    if not response and web_fallback:
+        if ddg.redirect.url:
+            is_fallback = True
+            found = True
+            response = ddg.redirect.url
+
+    # Final fallback
+    logger.info("No results!")
+    if not response:
+        found = False
+        is_fallback = True
+        response = 'Sorry, no results.'
+
+    logger.debug(f"Final response: {response!r}")
+    return (response, found, is_fallback)
